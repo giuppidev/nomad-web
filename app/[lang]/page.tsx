d97b7862e697de51228cacca951ca2416f259e60
Giuseppe Funicello <g.funicello@gmail.com>
2024-05-26 09:05:21 +0900

    Added: Modal Component | Test Version

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -3,6 +3,7 @@
 import { Locale } from "@/i18n.config";
 import Image from "next/image";
 // import { getDictionary } from "@/lib/dictionary";
+import { Modal } from "@/components/Modal";
 import { useLocaleContext } from "@/context/LocaleContext";
 import { DemoButtons } from "@/components/DemoButtons";
 
