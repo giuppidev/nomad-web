df13f2daca774f7161ef81f667bfe4e6b1f1ae31
Giuseppe Funicello <g.funicello@gmail.com>
2024-05-26 09:05:21 +0900

    Added: Modal Component | Test Version

---
+++ components/Modal/index.tsx
@@ -0,0 +1,33 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
+import { Button } from "@/components/ui/button";
+
+export function Modal(props) {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant="outline">Show Modal</Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>{props.Title}</AlertDialogTitle>
+          <AlertDialogDescription>{props.Description}</AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>{props.Cancel}</AlertDialogCancel>
+          <AlertDialogAction>{props.Action}</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
