d249563f21957a35aa1d415a3c570cbc1d2a0af2
Alberto Benatti <albi.benni8@gmail.com>
2024-06-21 20:58:52 +0200

    fix: update env db local variables

--- lib/migrate.ts
+++ lib/migrate.ts
@@ -5,7 +5,10 @@ import * as dotenv from "dotenv";
 
 dotenv.config({ path: "@/.env" });
 
-const connectionString = process.env.POSTGRES_URL || "";
+const postgresHost = process.env.DB_HOST || "localhost";
+const postgresPort = process.env.DB_PORT || "5432";
+
+const connectionString = `postgres://postgres:postgres@${postgresHost}:${postgresPort}/drizzle`;
 const sql = postgres(connectionString, { max: 1 });
 export const db = drizzle(sql);
 
